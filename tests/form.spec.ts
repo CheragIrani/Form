b38ac7216fbe083f1d64ca39218b36916d364911
CheragIrani <cfirani@hotmail.com>
2025-05-26 17:45:50 +0100

    navigate to form fixture added

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -1,4 +1,19 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
+
+const navigate = test.extend({
+  navigateToForm: async({ page }, use) => {
+
+    await page.goto('https://demoqa.com/automation-practice-form', {waitUntil: "domcontentloaded"});
+    await expect(async () => {
+      await expect(await page.getByRole('heading', { name: 'Practice Form' })).toBeVisible();
+      await expect(await page.getByRole('heading', { name: 'Student Registration Form' })).toBeVisible();
+
+    }).toPass()
+
+    await use(page)
+
+    }
+})
 
 test.afterEach('Close page', async({page}) => {
   await page.close()
@@ -15,13 +30,14 @@ test('Verify QA form page', async ({ page }) => {
   
   
 });
-test('Form can be submitted successfully', async ({ page }) => {
-  await page.goto('https://demoqa.com/automation-practice-form');
 
-  await page.getByRole('textbox', { name: 'First Name' }).fill('first');
-  await page.getByRole('textbox', { name: 'Last Name' }).fill('last');
-  await page.getByRole('textbox', { name: 'name@example.com' }).fill('name@example.com');
-  await page.getByText('Male', { exact: true }).click();
-  await page.getByRole('button', {name: 'Submit'}).click();
+navigate('Form can be submitted successfully', async ({ navigateToForm }) => {
+  await navigateToForm.goto('https://demoqa.com/automation-practice-form');
+
+  await navigateToForm.getByRole('textbox', { name: 'First Name' }).fill('first');
+  await navigateToForm.getByRole('textbox', { name: 'Last Name' }).fill('last');
+  await navigateToForm.getByRole('textbox', { name: 'name@example.com' }).fill('name@example.com');
+  await navigateToForm.getByText('Male', { exact: true }).click();
+  await navigateToForm.getByRole('button', {name: 'Submit'}).click();
   
 });
