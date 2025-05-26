fd724fd62cc09304c17ef97991722af2570e13ac
CheragIrani <cfirani@hotmail.com>
2025-05-26 17:54:40 +0100

    test removed from spec

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -15,25 +15,9 @@ const navigate = test.extend({
     }
 })
 
-test.afterEach('Close page', async({page}) => {
-  await page.close()
-})
-
-test('Verify QA form page', async ({ page }) => {
-  await page.goto('https://demoqa.com/automation-practice-form', {waitUntil: "domcontentloaded"});
-  
-  await expect(async () => {
-    await expect(await page.getByRole('heading', { name: 'Practice Form' })).toBeVisible();
-    await expect(await page.getByRole('heading', { name: 'Student Registration Form' })).toBeVisible();
-
-  }).toPass()
-  
-  
-});
 
 navigate('Form can be submitted successfully', async ({ navigateToForm }) => {
-  await navigateToForm.goto('https://demoqa.com/automation-practice-form');
-
+  
   await navigateToForm.getByRole('textbox', { name: 'First Name' }).fill('first');
   await navigateToForm.getByRole('textbox', { name: 'Last Name' }).fill('last');
   await navigateToForm.getByRole('textbox', { name: 'name@example.com' }).fill('name@example.com');
